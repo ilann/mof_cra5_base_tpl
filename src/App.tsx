75b681263fc2f51ca17561e221748e4bc4fcb6fd
foxi <ilan.fox@gmail.com>
2021-12-01 15:28:19 +0200

    css grid template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles.header}>Header</div>
+      <div className={styles.nav}>nav</div>
+      <div className={styles.sideBar}>Side bar</div>
+      <div className={styles.content}>Contetn</div>
+      <div className={styles.footer}>footer</div>
     </div>
   );
 }
